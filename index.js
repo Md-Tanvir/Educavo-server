1ec9187c7b1ebbe7cb8b878cbef46357ac2c0551
Md-Tanvir <contact.mdtanvirahmed@gmail.com>
2022-01-09 12:18:09 +0600

    bug fixed and made user collection

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b0w5j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = "mongodb+srv://tanvir10:tanvir190826@cluster0.b0w5j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -139,6 +136,12 @@ client.connect((err) => {
     res.send({ admin: isAdmin });
   });
 
+  // MAKING USER DATA
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+  });
 
 
 });
